8737236ddc675551d71d2690859f9917f5bf43f4
LIU FRANK <youquan.liu@hotmail.com>
2020-09-27 23:28:58 -0400

    1.cline side working 2.handle input change and submit on register page 3.flah input error message using toastcontainer

---
+++ server/client/src/Screens/Register.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+// import authSvg from '../assests/auth.svg';
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+import { authenticate, isAuth } from '../helpers/auth';
+import { Link, Redirect } from 'react-router-dom';
+
+const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password1: '',
+        password2: '',
+    })
+    const { name, email, password1, password2 } = formData
+
+    const handleChange = text => e => {
+        // console.log(name, email, password1, password2)
+        setFormData({ ...formData, [text]: e.target.value })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (name && email && password1) {
+            if (password1 === password2) {
+                axios.post(`${process.env.REACT_APP_API_URL}/register`, {
+                    name, email, password: password1
+                }).then(res => {
+                    setFormData({
+                        ...formData,
+                        name: '',
+                        email: '',
+                        password1: '',
+                        password2: ''
+                    });
+                    toast.success(res.data.message);
+                }).catch(err => {
+                    toast.error(err.response.data.error);
+                })
+            } else {
+                toast.error('passwords don\'t maches')
+            }
+        } else {
+            toast.error('Please fill all fields');
+        }
+    }
+    return (
+        <div className="">
+            {isAuth() ? <Redirect to='/' /> : null}
+            <ToastContainer />
+            <div>
+                <div>
+                    <h1>
+                        Sign Up for Their Faults
+                    </h1>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <input type="text" placeholder="Name"
+                                onChange={handleChange('name')} value={name}
+                            />
+                            <input type='email' placeholder='Email'
+                                onChange={handleChange('email')} value={email}
+                            />
+                            <input type="password" placeholder='Password'
+                                onChange={handleChange('password1')} value={password1}
+                            />
+                            <input type='password' placeholder='Confirm Password'
+                                onChange={handleChange('password2')} value={password2}
+                            />
+                            <button type='submit'>
+                                {/* <span>{textChange}</span> */}
+                                REGISTER
+                            </button>
+                        </div>
+                        <div>
+                            Sign in with email or Google
+                        </div>
+                        <div>
+                            <a href="/">Sign In</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Register from './Screens/Register';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact render={props => <App{...props} />} />
+      <Route path="/register" exact render={props => <Register{...props} />} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
